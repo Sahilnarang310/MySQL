7f406c32f21bc158ad5bf3ddc58a38335bd03058
Sahilnarang310 <softwaredev310@gamil.com>
2023-05-11 18:10:25 +0530

    Answer for task 17.

---
+++ task-17/util/database.js
@@ -0,0 +1,13 @@
+// const mysql=require('mysql2');
+// const pool=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'nodecomplete'
+// });
+// module.exports=pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('nodecomplete', 'root', '', {dialect:'mysql',host:'localhost'});
+
+module.exports=sequelize;
